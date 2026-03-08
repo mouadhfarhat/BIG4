4019bb0af98cbfc005add7a63cc3dba16132e557
Mouadh Farhat <fmouadh50@gmail.com>
2026-03-08 20:42:17 +0100

    cmt

--- BIG4/src/main/java/test/Launcher.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(MainFX.class, args);
-    }
-}
